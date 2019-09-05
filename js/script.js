3ce2c92fca1aefcb20e8b96cfb021e039aa5ec34
Collins Muriuki <murerwacollins@gmail.com>
2019-09-05 16:39:54 +0300

    add alert message on submission of subscription modal form

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-$(function () {
+//navbar
+$(function() {
   var header = $(".navbar");
-  $(window).scroll(function () {
+  $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll >= 50) {
       header.addClass("scrolled");
@@ -8,4 +9,20 @@ $(function () {
       header.removeClass("scrolled");
     }
   });
+});
+
+//subscribe to newsletter form
+$("#myForm").submit(function(event) {
+  event.preventDefault();
+  var userName = $("#name").val();
+  var userEmail = $("#email").val();
+
+  $("#response").text("Thank you " + userName + ", we shall get back to you.");
+  $("#registerModal").hide();
+  $('body').removeClass('modal-open');
+  $('.modal-backdrop').remove();
+  $("#myAlert").show();
+
+  $("#name").val();
+  $("#email").val();
 });
